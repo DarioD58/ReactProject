6701e7935bcaf3cae7f20e0e046e3aa25e04ed27
Dario Strunjak <dario.strunjak@fer.hr>
2020-10-14 18:56:02 +0200

    Nadogradnja baze

--- IzvorniKod/mlade_nade_create.sql
+++ IzvorniKod/mlade_nade_create.sql
@@ -1,33 +1,43 @@
+--dodati da je svaki id tipa SERIAL, za datum i vrijeme izvodenja dodati tip TIMESTAMP(0)
+
+--ekstenzija za kriptiranje lozinke korisnika
+CREATE EXTENSION pgcrypto;
+
+
 CREATE TABLE KAMP
 (
-  vrijeme_odrzavanja DATE NOT NULL,
+  ime_kamp VARCHAR(50) NOT NULL,
+  datum_odrzavanja_kamp DATE NOT NULL,
   trajanje_(dan/a) INT NOT NULL,
   pocetak_prijava_sudionika DATE NOT NULL,
   kraj_prijava_sudionika DATE NOT NULL,
   pocetak_prijava_animatora DATE NOT NULL,
   kraj_prijava_animatora DATE NOT NULL,
-  broj_grupa INT NOT NULL,
-  ime_kamp VARCHAR(50) NOT NULL,
-  PRIMARY KEY (ime_kamp)
+  broj_grupa INT,
+  PRIMARY KEY (datum_odrzavanja_kamp, ime_kamp)
 );
 
 CREATE TABLE AKTIVNOST
 (
+  id_aktivnost SERIAL NOT NULL,
   ime_aktivnost VARCHAR(100) NOT NULL,
   opis_aktivnost VARCHAR(500) NOT NULL,
   trajanje_aktivnost_(sat/i) INT NOT NULL,
   tip_aktivnost VARCHAR(20) NOT NULL,
-  PRIMARY KEY (ime_aktivnost)
+  datum_odrzavanja_kamp DATE NOT NULL,
+  ime_kamp VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id_aktivnost),
+  FOREIGN KEY (datum_odrzavanja_kamp, ime_kamp) REFERENCES KAMP(datum_odrzavanja_kamp, ime_kamp)
 );
 
 CREATE TABLE KORISNIK
 (
   korisnicko_ime VARCHAR(50) NOT NULL,
-  lozinka VARCHAR(50) NOT NULL,
+  lozinka VARCHAR(50),
   email VARCHAR(50) NOT NULL,
   ime VARCHAR(50) NOT NULL,
   prezime VARCHAR(100) NOT NULL,
-  status VARCHAR(50) NOT NULL,
+  status VARCHAR(50),
   PRIMARY KEY (korisnicko_ime),
   UNIQUE (email)
 );
@@ -41,22 +51,24 @@ CREATE TABLE ORGANIZATOR
 
 CREATE TABLE SUDIONIK
 (
-  br_tel_sudionik VARCHAR(20) NOT NULL,
-  datum_i_god_rod_sudionik DATE NOT NULL,
-  br_tel_odg_osobe VARCHAR(20) NOT NULL,
-  motivacijsko_pismo_sudionik VARCHAR(3000) NOT NULL,
   korisnicko_ime_sudionik VARCHAR(50) NOT NULL,
+  datum_i_god_rod_sudionik DATE NOT NULL,
+  br_tel_sudionik VARCHAR(20) NOT NULL,
+  motivacijsko_pismo_sudionik VARCHAR(3000) NOT NULL,
+  br_tel_odg_osobe VARCHAR(20) NOT NULL,
+  id_grupa INT,
   PRIMARY KEY (korisnicko_ime_sudionik),
   FOREIGN KEY (korisnicko_ime_sudionik) REFERENCES KORISNIK(korisnicko_ime),
+  FOREIGN KEY (id_grupa) REFERENCES GRUPA(id_grupa),
   UNIQUE (br_tel_sudionik)
 );
 
 CREATE TABLE ANIMATOR
 (
-  br_tel_animator VARCHAR(20) NOT NULL,
-  datum_i_god_rod_animator DATE NOT NULL,
-  motivacijsko_pismo_animator VARCHAR(3000) NOT NULL,
   korisnicko_ime_animator VARCHAR(50) NOT NULL,
+  datum_i_god_rod_animator DATE NOT NULL,
+  br_tel_animator VARCHAR(20) NOT NULL,
+  motivacijsko_pismo_animator VARCHAR(3000) NOT NULL,
   PRIMARY KEY (korisnicko_ime_animator),
   FOREIGN KEY (korisnicko_ime_animator) REFERENCES KORISNIK(korisnicko_ime),
   UNIQUE (br_tel_animator)
@@ -64,10 +76,10 @@ CREATE TABLE ANIMATOR
 
 CREATE TABLE PRIJAVA
 (
-  id_prijava INT NOT NULL,
-  vrijeme_prijava DATE NOT NULL,
-  status_prijava VARCHAR(20) NOT NULL,
+  id_prijava SERIAL NOT NULL,
   korisnicko_ime VARCHAR(50) NOT NULL,
+  datum_i_vrijeme_prijava TIMESTAMP(0) NOT NULL,
+  status_prijava VARCHAR(20),
   PRIMARY KEY (id_prijava),
   FOREIGN KEY (korisnicko_ime) REFERENCES KORISNIK(korisnicko_ime)
 );
@@ -75,41 +87,47 @@ CREATE TABLE PRIJAVA
 CREATE TABLE GRUPA
 (
   ime_grupa VARCHAR(50) NOT NULL,
-  korisnicko_ime_sudionik VARCHAR(50) NOT NULL,
-  PRIMARY KEY (ime_grupa),
-  FOREIGN KEY (korisnicko_ime_sudionik) REFERENCES SUDIONIK(korisnicko_ime_sudionik)
+  id_grupa  NOT NULL,
+  PRIMARY KEY (id_grupa)
 );
 
 CREATE TABLE RASPORED
 (
   datum_i_vrijeme_izvrsavanja TIMESTAMP(0) NOT NULL,
-  ime_grupa VARCHAR(50) NOT NULL,
-  ime_aktivnost VARCHAR(100) NOT NULL,
+  id_aktivnost INT NOT NULL,
+  id_grupa INT NOT NULL,
   korisnicko_ime_animator VARCHAR(50) NOT NULL,
-  PRIMARY KEY (datum_i_vrijeme_izvrsavanja, ime_grupa, ime_aktivnost),
-  FOREIGN KEY (ime_grupa) REFERENCES GRUPA(ime_grupa),
-  FOREIGN KEY (ime_aktivnost) REFERENCES AKTIVNOST(ime_aktivnost),
-  FOREIGN KEY (korisnicko_ime_animator) REFERENCES ANIMATOR(korisnicko_ime_animator)
+  PRIMARY KEY (datum_i_vrijeme_izvrsavanja, id_aktivnost, id_grupa),
+  FOREIGN KEY (korisnicko_ime_animator) REFERENCES ANIMATOR(korisnicko_ime_animator),
+  FOREIGN KEY (id_aktivnost) REFERENCES AKTIVNOST(id_aktivnost),
+  FOREIGN KEY (id_grupa) REFERENCES GRUPA(id_grupa)
+);
+
+CREATE TABLE SJEDNICA
+(
+  id_sjednica VARCHAR NOT NULL,
+  podatci JSON NOT NULL,
+  rok_trajanja TIMESTAMP(0) NOT NULL
 );
 
 CREATE TABLE animator_ocjena_aktivnosti
 (
+  korisnicko_ime_animator VARCHAR(50) NOT NULL,
+  id_aktivnost INT NOT NULL,
   ocjena_animator INT NOT NULL,
   dojam_animator VARCHAR(500) NOT NULL,
-  korisnicko_ime_animator VARCHAR(50) NOT NULL,
-  ime_aktivnost VARCHAR(100) NOT NULL,
-  PRIMARY KEY (korisnicko_ime_animator, ime_aktivnost),
+  PRIMARY KEY (korisnicko_ime_animator),
   FOREIGN KEY (korisnicko_ime_animator) REFERENCES ANIMATOR(korisnicko_ime_animator),
-  FOREIGN KEY (ime_aktivnost) REFERENCES AKTIVNOST(ime_aktivnost)
+  FOREIGN KEY (id_aktivnost) REFERENCES AKTIVNOST(id_aktivnost)
 );
 
 CREATE TABLE sudionik_ocjena_aktivnosti
 (
+  korisnicko_ime_sudionik VARCHAR(50) NOT NULL,
+  id_aktivnost INT NOT NULL,
   ocjena_sudionik INT NOT NULL,
   dojam_sudionik VARCHAR(500) NOT NULL,
-  korisnicko_ime_sudionik VARCHAR(50) NOT NULL,
-  ime_aktivnost VARCHAR(100) NOT NULL,
-  PRIMARY KEY (korisnicko_ime_sudionik, ime_aktivnost),
+  PRIMARY KEY (korisnicko_ime_sudionik),
   FOREIGN KEY (korisnicko_ime_sudionik) REFERENCES SUDIONIK(korisnicko_ime_sudionik),
-  FOREIGN KEY (ime_aktivnost) REFERENCES AKTIVNOST(ime_aktivnost)
+  FOREIGN KEY (id_aktivnost) REFERENCES AKTIVNOST(id_aktivnost)
 );

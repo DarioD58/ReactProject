28916eaa3fb991f7532401139f0bd7a94e199b25
Dario Dekovic <dekovic.dario@gmail.com>
2020-11-07 00:15:45 +0100

    stvoren osnovni template za stranicu

---
+++ IzvorniKod/React/kamp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="everything">
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ IzvorniKod/React/kamp/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <header id="header" className="container-fluid bg-dark pt-3 pb-3">
+        <img src=".\slike\logo.png" alt="Slika kampa" className="rounded-circle mx-auto d-block"/>
+      </header>
+    );
+  }
+  
+  export default Header;

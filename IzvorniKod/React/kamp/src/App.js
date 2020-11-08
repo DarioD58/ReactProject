dc6e2f3e5e8bd62c36183f240b4cbf41b4bc4991
Toni_Dujmovic <toni.dujmovic@fer.hr>
2020-11-08 17:47:16 +0100

    Nadodani countdown, kalendar, sidebar i gumbovi

--- IzvorniKod/React/kamp/src/App.js
+++ IzvorniKod/React/kamp/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Countdown from './components/Countdown';
+import NotButton from './components/NotButton';
+import Calendar from 'react-calendar';
 import './App.css'
+import 'react-calendar/dist/Calendar.css'
+import Sidebar from './components/Sidebar';
+
 
 
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Header />
       <div className="everything">
+        <NotButton />
+        <Countdown />
+        <Calendar />
       </div>
       <Footer />
     </div>

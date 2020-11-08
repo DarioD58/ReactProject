4abc1707c575157ce554cb2fef01b822ab05e00f
Toni_Dujmovic <toni.dujmovic@fer.hr>
2020-11-08 17:49:47 +0100

    Nadodana polja za prijavu i registraciju

---
+++ IzvorniKod/React/kamp/src/components/NotButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotButton(){
+    return (
+        <div>
+            <div className="login-not-button text-light">Registriraj se</div>
+            <div className="text-light center text-center">Već si registriran prijavi se!</div>
+        </div>
+    );
+}
+export default NotButton;

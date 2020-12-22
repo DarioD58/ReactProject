ca72579a72666d9bd22c8286ba33189b4a65dcc8
Tin <>
2020-12-22 21:43:46 +0100

    dodane osnovne metode za pristup bazi

---
+++ IzvorniKod/Kamp_Mlade_nade/db/metode.js
@@ -0,0 +1,12 @@
+//dohvat pojedine grupe
+function dohvatGrupe(ime_kamp){
+
+}
+
+//izmjena podataka grupe
+
+//izmjena grupe sudionika
+
+//dohvat svih grupa na trenutnom kampu
+
+//metoda za unos nove grupe

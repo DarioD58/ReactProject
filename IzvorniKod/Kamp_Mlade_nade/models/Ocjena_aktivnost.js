19c96680268de4005331b736ec5ce86949338ada
Tin <>
2021-01-03 14:02:10 +0100

    popravljene sintaksne greske

--- IzvorniKod/Kamp_Mlade_nade/models/Ocjena_aktivnost.js
+++ IzvorniKod/Kamp_Mlade_nade/models/Ocjena_aktivnost.js
@@ -13,6 +13,44 @@ module.exports = class Ocjena_aktivnost {
     // metoda za unos ocjene aktivnosti
     // Poziva se nad instancom razreda Ocjena_aktivnost
     async addNewOcjenaAktivnost(){
+        try {
+            let ocjena = await dbAddNewOcjenaAktivnost(this);
+            this.ocjena = ocjena;
+        } catch(err){
+            console.log(err);
+            throw err;
+        }
+    }
+    //metoda za unos dojma o aktivnosti
+    async addNewDojamAktivnost(){
+        try {
+            let dojam = await dbAddNewDojamAktivnost(this);
+            this.dojam = dojam;
+        } catch(err){
+            console.log(err);
+            throw err;
+        }
+    }
+}
 
+dbAddNewOcjenaAktivnost = async (ocjena) => {
+    const sql = 'INSERT INTO sudionik_ocjena_aktivnosti(korisnicko_ime_sudionik, ocjena_sudionik) VALUES (' + this.korisnicko_ime + ',' + ocjena + ')';
+    try {
+        const result = await db.query(sql, [ocjena]);
+        return result.rows;
+    } catch(err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+dbAddNewDojamAktivnost = async (dojam) => {
+    const sql = 'INSERT INTO sudionik_ocjena_aktivnosti(korisnicko_ime_sudionik, dojam_sudionik) VALUES (' + this.korisnicko_ime + ',' + dojam + ')';
+    try {
+        const result = await db.query(sql, [dojam]);
+        return result.rows;
+    } catch(err) {
+        console.log(err);
+        throw err;
     }
 }

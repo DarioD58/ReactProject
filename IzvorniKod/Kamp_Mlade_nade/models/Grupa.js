59137123b836b73b67206202ff4eb9efc01655a7
Tin <>
2021-01-03 14:42:34 +0100

    dodane funkcionalnosti promjene imena grupe

--- IzvorniKod/Kamp_Mlade_nade/models/Grupa.js
+++ IzvorniKod/Kamp_Mlade_nade/models/Grupa.js
@@ -25,17 +25,16 @@ module.exports = class Grupa {
     }
 
     // dohvaća sve članove grupe jedne grupe: iz tablice sudionik i grupa spojiti po id_grupa
-    // PAZI! Polje nije popunjeno
+    // PAZI! Polje nije popunjeno //+
     async getAllMembers(id_grupa){
       let results = await dbGetAllMembers(id_grupa);
       let clanovi = [];
       if (results.length > 0){
         for(let i = 0; i < results.length; i++){
           let clan = new Korisnik(results[i].korisnicko_ime, results[i].lozinka)
+          clanovi.push(clan);
         }
       }
-
-
     }
 
 
@@ -44,6 +43,11 @@ module.exports = class Grupa {
         // u petlji napraviti traženi broj grupa i dodavati jednu po jednu u bazu
         // u tablicu Kamp u bazi odgovarajucem kampu dodati broj grupa
         // podijeliti sudionike po grupama tako da broj sudionika u grupi bude podjednak
+    }
+
+    //metoda za izmjenu imena grupe
+    async changeGroupName(id_grupa){
+      dbChangeGroupName(id_grupa, novo_ime_grupe);
     }
 
 }
@@ -70,6 +74,17 @@ try{
   const result = await db.query(sql, [sudionik.korisnicko_ime_sudionik]);
   return result.rows;
   } catch(err){
+    console.log(err);
+    throw err;
+  }
+}
+
+dbChangeGroupName = async (id_grupa, novo_ime_grupe) => {
+  const sql = 'UPDATE GRUPA SET ime_grupa = ' + novo_ime_grupe + 'WHERE id_grupa = ' + id_grupa;
+  try {
+    const result = await db.query(sql, [id_grupa]);
+    return result.rows;
+  } catch (err) {
     console.log(err);
     throw err;
   }

a665e62dd50318f47a66c23edd432fd35ed13cbe
Katarina Dolić <kd51182@fer.hr>
2021-01-03 15:29:55 +0000

    Ocjena_aktivnosti.js db metode

--- IzvorniKod/Kamp_Mlade_nade/models/Ocjena_aktivnost.js
+++ IzvorniKod/Kamp_Mlade_nade/models/Ocjena_aktivnost.js
@@ -54,3 +54,43 @@ dbAddNewDojamAktivnost = async (dojam) => {
         throw err;
     }
 }
+
+dbGetOcjenaKorisnik = async(korisnicko_ime) =>{
+    const sql = `SELECT ocjena, dojam, id_aktivnost
+    FROM Ocjena_aktivnost WHERE korisnicko_ime LIKE '$1'`;
+    try {
+        const result = await db.query(sql);
+        return result.rows;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+dbGetOcjenaGrupa = async(id_grupa) =>{
+    const sql = `SELECT ocjena, dojam, id_aktivnost, korisnicko_ime
+    FROM Ocjena_aktivnost JOIN korisnik ON korisnicko_ime_sudionik=korisnicko_ime 
+    NATURAL JOIN raspored WHERE raspored.id_grupa = $1`;
+    try {
+        const result = await db.query(sql);
+        return result.rows;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+dbGetOcjenaAcitvities = async (id_aktivnost) => {
+    const sql = `SELECT ime_aktivnost, ocjena, dojam, id_aktivnost, korisnicko_ime
+    FROM Ocjena_aktivnost JOIN korisnik ON korisnicko_ime_sudionik=korisnicko_ime 
+    NATURAL JOIN raspored WHERE aktivnost.id_aktivnost LIKE $1`;
+    try {
+        const result = await db.query(sql, [id_grupa]);
+        return result.rows;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+

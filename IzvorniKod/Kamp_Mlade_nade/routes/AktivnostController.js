a03e7757bed6a2cb1caf47602993830382f8fd11
Dario Strunjak <dario.strunjak@fer.hr>
2021-01-12 18:00:49 +0100

    dojam

--- IzvorniKod/Kamp_Mlade_nade/routes/AktivnostController.js
+++ IzvorniKod/Kamp_Mlade_nade/routes/AktivnostController.js
@@ -54,7 +54,8 @@ class AktivnostController extends Controller {
 
     async activityGrade(req, res, next) {
         let korisnik = JSON.parse(req.cookies.korisnik);
-        
+        console.log(korisnik)
+        console.log(req.body)
         try {
             let ocjena_aktivnost = new Ocjena_aktivnost(req.body.ocjena, req.body.dojam, req.body.id_aktivnost, korisnik.korisnickoIme);
             await ocjena_aktivnost.addNewOcjenaAktivnost();

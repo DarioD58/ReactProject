bc1ed8cd2f8de84ad981ca4b12d83797a8b050e9
Dario Strunjak <dario.strunjak@fer.hr>
2021-01-13 15:45:44 +0100

    Deploy testing

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const {Pool} = require('pg');
+/* const {Pool} = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
@@ -6,11 +6,31 @@ const pool = new Pool({
     database: 'kampAdmin',
     password: 'bazepodataka', //svatko svoju sifru za sada
     port: 5432,
+}); */
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+client.connect();
+
+/* client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+}); */
+
 module.exports = {
     query: (text, params) => {
         const start = Date.now();
-        return pool.query(text, params)
+        return client.query(text, params)
             .then(res => {
                 const duration = Date.now() - start;
                 //console.log('executed query', {text, params, duration, rows: res.rows});

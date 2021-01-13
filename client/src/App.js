13d08d961addc635d0f62519d2e60504965d7e9b
Dario Strunjak <dario.strunjak@fer.hr>
2021-01-13 19:00:39 +0100

    bilo šta

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,7 @@ function App() {
       setSession('true')
     }
     // GET request using fetch inside useEffect React hook
-    fetch('/', {
+    fetch('http://localhost:5000/', {
       credentials: 'include',
       method: 'GET'
     })
